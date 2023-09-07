1a08b83b0abd28f87ac38e06ec56f4d29b6ba225
Jessica Fellows <jessicaxfellows@gmail.com>
2023-09-07 19:05:04 -0400

    Python

---
+++ GeneticAlgo.py
@@ -0,0 +1,74 @@
+import random
+
+#Constants
+TARGET_PHRASE = "Nami is a bear" #the target phrase to be matched
+POPULATION_SIZE = 250 #number of individuals in the population
+MUTATION_RATE = 0.02 #probability of mutation
+
+#Generate initial population 
+def generate_population():
+    population = []
+    for _ in range(POPULATION_SIZE):
+        individual = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ,.!') for _ in range(len(TARGET_PHRASE)))
+        population.append(individual)
+    return population
+
+#Calculate fitness score
+def calculate_fitness(individual):
+    score = 0
+    for i in range(len(TARGET_PHRASE)):
+        if individual[i] == TARGET_PHRASE[i]:
+            score += 1
+    return score
+
+#Select parents based in fitness
+def select_parents(population):
+    parents = []
+    for _ in range(2):
+        parents.append(max(population, key=calculate_fitness))
+    return parents
+
+#Create offspring through crossover
+def crossover(parents):
+    offspring = ""
+    crossover_point = random.randint(0, len(TARGET_PHRASE) - 1)
+    for i in range(len(TARGET_PHRASE)):
+        if i <= crossover_point:
+            offspring += parents[0][i]
+        else:
+            offspring += parents[1][i]
+    return offspring
+
+#Mutate offspring
+def mutate(offspring):
+    mutated_offspring = ""
+    for i in range(len(offspring)):
+        if random.random() < MUTATION_RATE:
+            mutated_offspring += random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ,.!')
+        else:
+            mutated_offspring += offspring[i]
+    return mutated_offspring
+
+#Main genetic algo
+def genetic_algorithm():
+    population = generate_population()
+    generation = 1
+
+    while True:
+        print(f"Generation {generation} - Best Fit: {max(population, key=calculate_fitness)}")
+
+        if TARGET_PHRASE in population:
+            break
+
+        new_population = []
+        for _ in range(POPULATION_SIZE // 2):
+            parents = select_parents(population)
+            offspring = crossover(parents)
+            mutated_offspring = mutate(offspring)
+            new_population.extend([offspring, mutated_offspring])
+        
+        population = new_population
+        generation +=1
+
+#Run the genetic algo
+genetic_algorithm()
